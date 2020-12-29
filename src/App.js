1947aad7e9aac4e323ecf6f8f2fdc826aaa33d1d
N0mercy00 <dkdhehesp@naver.com>
2020-12-29 19:20:18 +0900

    reuse by map JS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
-function Food({fav}){
-  return (<h3>i love {fav} </h3>);
+function Food({name,picture}){
+  return (<div>
+    <h2>I like {name}</h2>
+    <img src={picture}/>
+  </div>);
 }
+
+const foodILike=[
+  {
+    name:"hamburger",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg/330px-NYC-Diner-Bacon-Cheeseburger.jpg"
+  },
+  {
+    name:"pizza",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Supreme_pizza.jpg/330px-Supreme_pizza.jpg"
+  }
+]
 
 function App() {
   return (
     <div>
       <h1>hello!!</h1>
-     <Food fav ="kimchi"/>
-     <Food fav ="porkbelly"/>
-     <Food fav ="Galbi"/>
+    {foodILike.map(dish=>(
+      <Food name={dish.name} picture={dish.image}/>
+     ))}
     </div>
   );
 }
