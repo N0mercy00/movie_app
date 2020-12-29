a5879bd3057272706d8d6170ed78c3990da6aa2b
N0mercy00 <dkdhehesp@naver.com>
2020-12-29 20:01:55 +0900

    component and prop 2

--- src/App.js
+++ src/App.js
@@ -3,28 +3,31 @@ import React from 'react';
 function Food({name,picture}){
   return (<div>
     <h2>I like {name}</h2>
-    <img src={picture}/>
+    <img src={picture} alt ={name}/>
   </div>);
 }
 
 const foodILike=[
   {
+    id:1,
     name:"hamburger",
     image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg/330px-NYC-Diner-Bacon-Cheeseburger.jpg"
   },
   {
+    id:2,
     name:"pizza",
     image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Supreme_pizza.jpg/330px-Supreme_pizza.jpg"
   }
 ]
 
+
+
 function App() {
   return (
     <div>
-      <h1>hello!!</h1>
     {foodILike.map(dish=>(
-      <Food name={dish.name} picture={dish.image}/>
-     ))}
+      <Food key={dish.id} name={dish.name} picture={dish.image}/>
+    ))}
     </div>
   );
 }
